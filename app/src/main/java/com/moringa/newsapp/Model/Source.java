05a82b8688aa39c746bffed4a0bad2def461b2d4
BTG001 <brunokipkorir2@gmail.com>
2020-08-09 16:25:42 +0300

    Convert to respective Java types in the Source class

---
+++ app/src/main/java/com/moringa/newsapp/Model/Source.java
@@ -0,0 +1,15 @@
+package com.moringa.newsapp.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Source {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+}
