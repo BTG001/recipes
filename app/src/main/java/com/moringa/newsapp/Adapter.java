daf89e60d266ef02bf50d5adfa3edd9c7ea675a8
BTG001 <brunokipkorir2@gmail.com>
2020-08-10 02:50:36 +0300

    Include a constructor for Adapter class

--- app/src/main/java/com/moringa/newsapp/Adapter.java
+++ app/src/main/java/com/moringa/newsapp/Adapter.java
@@ -1,12 +1,29 @@
 package com.moringa.newsapp;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.moringa.newsapp.Model.Articles;
+
+import java.util.List;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    Context context;
+    List<Articles> articles;
+
+    public Adapter(Context context, List<Articles> articles) {
+        this.context = context;
+        this.articles = articles;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -24,8 +41,19 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle, tvSource, tvDate;
+        ImageView imageView;
+        CardView cardView;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvSource = itemView.findViewById(R.id.tvSource);
+            tvDate = itemView.findViewById(R.id.tvDate);
+            imageView = itemView.findViewById(R.id.image);
+            cardView = itemView.findViewById(R.id.cardView);
+
         }
     }
 }
