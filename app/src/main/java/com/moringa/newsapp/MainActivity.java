bfd529645de48f94b7f3e04a53b361bf7ab092c4
BTG001 <brunokipkorir2@gmail.com>
2020-08-10 03:41:59 +0300

    Get appropriate feedback to the user when the network request is ongoing

--- app/src/main/java/com/moringa/newsapp/MainActivity.java
+++ app/src/main/java/com/moringa/newsapp/MainActivity.java
@@ -1,14 +1,59 @@
 package com.moringa.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.moringa.newsapp.Model.Articles;
+import com.moringa.newsapp.Model.Headlines;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    final String API_KEY = "7394aad3dd1e4109a86f2d5b9185812f";
+    Adapter adapter;
+    List<Articles> articles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
+
+    public void retrieveJson(String country, String apiKey) {
+
+        Call<Headlines> call = ApiClient.getInstance().getApi().getHeadlines(country, apiKey);
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if (response.isSuccessful() )
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public String getCountry() {
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+
 }
