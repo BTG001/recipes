1bcba645432a7be84a3e81a3f708c8f507eb9032
BTG001 <brunokipkorir2@gmail.com>
2020-08-17 12:38:08 +0300

    Tell the app what action to perform when the user selects the logout option.

--- app/src/main/java/com/moringa/newsapp/MainActivity.java
+++ app/src/main/java/com/moringa/newsapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -103,6 +104,16 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
