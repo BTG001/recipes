db14f89db162891182d002e2956ae171c2a8c966
BTG001 <brunokipkorir2@gmail.com>
2020-08-18 07:52:12 +0300

    Add a click listener to get details once the user fills te form

--- app/src/main/java/com/moringa/newsapp/CreateAccountActivity.java
+++ app/src/main/java/com/moringa/newsapp/CreateAccountActivity.java
@@ -3,6 +3,7 @@ package com.moringa.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +21,16 @@ public class CreateAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        mFirebaseAuth = FirebaseAuth.getInstance()
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+        btnSignUp = findViewById(R.id.button2);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+            }
+        });
     }
 }
