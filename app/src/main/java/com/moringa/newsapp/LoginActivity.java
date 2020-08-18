e38e7d565a1be2967e7abcfab940e5e3561c382c
BTG001 <brunokipkorir2@gmail.com>
2020-08-18 13:03:45 +0300

    Use Firebase authentication listener to check whether the user email id and password exist. If it exist we can do login

--- app/src/main/java/com/moringa/newsapp/LoginActivity.java
+++ app/src/main/java/com/moringa/newsapp/LoginActivity.java
@@ -3,12 +3,15 @@ package com.moringa.newsapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     EditText emailId, password;
@@ -32,7 +35,12 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if (mFirebaseUser !=null ) {
+                    Toast.makeText(LoginActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
             }
         }
     }
