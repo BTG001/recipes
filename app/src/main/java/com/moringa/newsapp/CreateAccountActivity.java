4614fee807b3b34369aabe9073e51c4941d2fdec
BTG001 <brunokipkorir2@gmail.com>
2020-08-18 07:46:58 +0300

    Get instance of FirebaseAuth

--- app/src/main/java/com/moringa/newsapp/CreateAccountActivity.java
+++ app/src/main/java/com/moringa/newsapp/CreateAccountActivity.java
@@ -3,12 +3,23 @@ package com.moringa.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
+    EditText emailId, password;
+    Button btnSignUp;
+    TextView tvSignIn;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        mFirebaseAuth = FirebaseAuth.getInstance()
     }
 }
